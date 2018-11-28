c2f44c96b7894a75d9046ac66e270c375b8386cc
KostyaOrlov <kostiantyn.orlov@limestonedigital.co>
2018-11-28 18:30:46 +0200

    added postbuild script for production

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -12,19 +12,21 @@ export const getItems = () => dispatch => {
 };
 
 export const addItem = item => dispatch => {
-  axios.post('/api/items', item)
-  .then(res=> dispatch({
+  axios.post("/api/items", item).then(res =>
+    dispatch({
       type: ADD_ITEM,
       payload: res.data
-  }))
+    })
+  );
 };
 
 export const deleteItem = id => dispatch => {
-  axios.delete(`/api/items/${id}`)
-  .then(res => dispatch({
-type: DELETE_ITEM,
-payload: id
-  }))
+  axios.delete(`/api/items/${id}`).then(res =>
+    dispatch({
+      type: DELETE_ITEM,
+      payload: id
+    })
+  );
 };
 
 export const setItemsLoading = () => {
